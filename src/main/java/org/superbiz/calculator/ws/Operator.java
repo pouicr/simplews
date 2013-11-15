fc4aafbf0e8b612c166339b055d20015551cb00b
Loic Rodier <loic.rodier@atos.net>
2013-11-15 05:48:06 -0500

    update ex

---
+++ src/main/java/org/superbiz/calculator/ws/Operator.java
@@ -0,0 +1,14 @@
+package org.superbiz.calculator.ws;
+
+/**
+ */
+public class Operator {
+
+    public int sum(int add1, int add2) {
+        return add1 + add2;
+    }
+
+    public int multiply(int mul1, int mul2) {
+        return mul1 * mul2;
+    }
+}
